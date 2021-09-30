419dc9bf5a58e318bd945a083704cfda779c3748
JorneSchuurmans <jorne.schuurmans@student.ucll.be>
2021-09-30 14:16:01 +0200

    Uart Construct

---
+++ RondleidingRobot/Uart/UartPacket.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RondleidingRobot.Uart
+{
+    public abstract class UartPacket
+    {
+        public static char[] Construct(char cmd, char[] args) 
+        {
+            char[] pakket = new char[args.Length + 5];
+            pakket[0] = (char)2;
+            pakket[1] = (char)(args.Length + 1);
+            pakket[2] = cmd;
+            int crc = (int)cmd;
+            int teller = 3;
+            foreach (char c in args) 
+            {
+                crc += (int)c;
+                pakket[teller++] = c;
+            }
+            pakket[pakket.Length - 2] = (char)(crc%255);
+            pakket[pakket.Length - 1] = (char)3;
+            return pakket;
+        }
+    }
+}
---
+++ RondleidingRobotTest/Uart/UartPacketTest.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RondleidingRobotTest.Uart
+{
+    [TestClass]
+    public class UartPacketTest
+    {
+        [TestMethod]
+        public void ContructTest1()
+        {
+            char cmd = 'v';
+            char[] args = { 'a', 'b', 'c'};
+            char[] expected = { (char)2, (char)4, 'v','a','b','c', (char)157, (char)3};
+            char[] result = RondleidingRobot.Uart.UartPacket.Construct(cmd,args);
+            CollectionAssert.AreEqual(expected,result);
+        }
+
+        [TestMethod]
+        public void ContructTest2()
+        {
+            char cmd = 'r';
+            char[] args = { 'x'};
+            char[] expected = { (char)2, (char)2, 'r', 'x', (char)234, (char)3 };
+            char[] result = RondleidingRobot.Uart.UartPacket.Construct(cmd, args);
+            CollectionAssert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void ContructTest3()
+        {
+            char cmd = 's';
+            char[] args = {};
+            char[] expected = { (char)2, (char)1, 's', 's', (char)3 };
+            char[] result = RondleidingRobot.Uart.UartPacket.Construct(cmd, args);
+            CollectionAssert.AreEqual(expected, result);
+        }
+    }
+}
